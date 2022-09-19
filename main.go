c7a0182a97b51490375c7848dcfde0f4666f66df
jesus m. rodriguez <jesusr@redhat.com>
2022-09-18 21:41:05 -0400

    cobra-cli init

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
+/*
+Copyright © 2022 jesus m. rodriguez jmrodri@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"github.com/google/go-github/v47/github"
-
-	"github.com/jmrodri/gh2jira/internal/gh"
-)
+import "github.com/jmrodri/gh2jira/cmd"
 
 // So we will want to allow this to be able to take in a specific GH issue id or
 // --all.
@@ -19,9 +30,5 @@ import (
 // gh2jira list --project operator-framework/operator-sdk [--milestone=] [--assignee=]
 // gh2jira copy GH# [--dry-run]
 func main() {
-	gh.GetGithubIssues()
-	CreateJiraIssue(nil)
-}
-
-func CreateJiraIssue(issue *github.Issue) {
+	cmd.Execute()
 }
